192639aa2ca233a5c930c5d857a7704d8b584248
RomanArzate010 <86375645+RomanArzate010@users.noreply.github.com>
2021-06-23 14:03:06 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Git Pull</h1>
+<div>
+  Git pull is a way to get commands and download content from a repository.
+</div>
+
+<h1>Git Clone</h1>
+<div>
+ making a copy of your last repository
+ </div>
+`;
